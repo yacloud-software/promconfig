5b1e07b4abbce2c83359f0ef38a4b4d56f3c387a
Conrad Wood <cnw@conradwood.net>
2022-04-28 17:57:54 +0100

    .

--- src/golang.conradwood.net/promconfig/client/promconfig-client.go
+++ src/golang.conradwood.net/promconfig/client/promconfig-client.go
@@ -4,10 +4,15 @@ import (
 	"flag"
 	"fmt"
 	pb "golang.conradwood.net/apis/promconfig"
+	"golang.conradwood.net/go-easyops/authremote"
 	"golang.conradwood.net/go-easyops/client"
-	"golang.conradwood.net/go-easyops/tokens"
 	"golang.conradwood.net/go-easyops/utils"
 	"os"
+	"strings"
+)
+
+var (
+	find = flag.String("find", "", "find a given series")
 )
 
 // this is a very simple client.
@@ -18,14 +23,17 @@ import (
 // terribly useful.
 func main() {
 	flag.Parse()
-
+	if *find != "" {
+		Find()
+		os.Exit(0)
+	}
 	// get a Connection
 	// (also accepts a path, such as "picoservices/authserver/1" )
 	con := client.Connect("promconfig.PromConfigService")
 	// once we got a connection, here's the 'client'
 	echoClient := pb.NewPromConfigServiceClient(con)
 	// a context with authentication
-	ctx := tokens.ContextWithToken()
+	ctx := authremote.Context()
 
 	empty := pb.TargetList{}
 	response, err := echoClient.NewTargets(ctx, &empty)
@@ -34,4 +42,23 @@ func main() {
 
 	fmt.Printf("Done.\n")
 	os.Exit(0)
+}
+func Find() {
+	sm := buildMatcher(*find)
+	ctx := authremote.Context()
+	sl, err := pb.GetPromConfigServiceClient().FindSeries(ctx, sm)
+	utils.Bail("failed to get series", err)
+	fmt.Printf("Got %d series\n", len(sl.Series))
+	for _, s := range sl.Series {
+		fmt.Printf("%s\n", s)
+	}
+}
+
+func buildMatcher(s string) *pb.SeriesMatch {
+	ms := strings.Split(s, ",")
+	res := &pb.SeriesMatch{PartialMatch: true}
+	for _, m := range ms {
+		res.Prefix = append(res.Prefix, m)
+	}
+	return res
 }
--- src/golang.conradwood.net/promconfig/server/query.go
+++ src/golang.conradwood.net/promconfig/server/query.go
@@ -2,13 +2,82 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"flag"
+	"golang.conradwood.net/apis/common"
 	pb "golang.conradwood.net/apis/promconfig"
+	"golang.conradwood.net/go-easyops/http"
+	"strings"
+)
+
+var (
+	api_url = flag.String("prometheus_api_url", "http://prometheus:9090/prometheus/api/v1/", "url of the prometheus server")
 )
 
 /*
 curl 'http://prometheus:9090/prometheus/api/v1/label/__name__/values'
 */
-func (p *promConfigServer) GetSeries(ctx context.Context, req *pb.SeriesMatch) (*pb.SeriesList, error) {
+func (p *promConfigServer) FindSeries(ctx context.Context, req *pb.SeriesMatch) (*pb.SeriesList, error) {
+	sl, err := getAllSeries()
+	if err != nil {
+		return nil, err
+	}
 	res := &pb.SeriesList{}
+	for _, s := range sl.Series {
+		if seriesMatches(req, s) {
+			res.Series = append(res.Series, s)
+		}
+	}
+	return res, nil
+}
+func (p *promConfigServer) GetSeries(ctx context.Context, req *common.Void) (*pb.SeriesList, error) {
+	res, err := getAllSeries()
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// does a series match a given matcher?
+func seriesMatches(req *pb.SeriesMatch, s *pb.Series) bool {
+	t := strings.ToLower(s.Name)
+	for _, p := range req.Prefix {
+		p = strings.ToLower(p)
+		if req.PartialMatch {
+			if strings.Contains(t, p) {
+				return true
+			}
+		} else {
+			if strings.HasPrefix(t, p) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+type PromJsonSeries struct {
+	Status   string   `json:"status"`
+	Data     []string `json:"data"`
+	Warnings []string `json:"warnings"`
+}
+
+func getAllSeries() (*pb.SeriesList, error) {
+	ht := &http.HTTP{}
+	hr := ht.Get(*api_url + "label/__name__/values")
+	err := hr.Error()
+	if err != nil {
+		return nil, err
+	}
+	pjs := &PromJsonSeries{}
+	err = json.Unmarshal(hr.Body(), pjs)
+	if err != nil {
+		return nil, err
+	}
+	res := &pb.SeriesList{}
+	for _, d := range pjs.Data {
+		s := &pb.Series{Name: d}
+		res.Series = append(res.Series, s)
+	}
 	return res, nil
 }
