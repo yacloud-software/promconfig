9f9ca58f14e8c49f76b8ef3a094312cdd8e23140
Conrad Wood <cnw@conradwood.net>
2023-01-11 15:09:21 +0000

    .
    Change-Id: Ic38cca9105d42b3c3f91ab87be9299091df8a725

---
+++ src/golang.conradwood.net/promconfig/server/htmlrender.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"golang.conradwood.net/apis/htmlserver"
+)
+
+func (e *promConfigServer) HTMLRenderer(ctx context.Context, req *htmlserver.SnippetRequest) (*htmlserver.SnippetResponse, error) {
+	fmt.Printf("Serving \"%s\"\n", req.Path)
+	sr := &htmlserver.SnippetResponse{
+		Body:        []byte("foobody"),
+		DoNotModify: true,
+	}
+	return sr, nil
+}
