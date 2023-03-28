08cd9f63729f28fa0e682adefcb9074a13547e48
Conrad Wood <cnw@conradwood.net>
2023-03-28 07:27:42 +0100

    updated to go-easyops version 16845
    Change-Id: I8cca13ae8f0a5d2d1f4a251752476608809a1c5e

--- src/golang.conradwood.net/apis/promconfig/create_1.go
+++ src/golang.conradwood.net/apis/promconfig/create_1.go
@@ -1,8 +1,15 @@
 // client create: PromConfigServiceClient
+/*
+  Created by /home/cnw/devel/go/go-tools/src/golang.conradwood.net/gotools/protoc-gen-cnw/protoc-gen-cnw.go
+*/
+
 /* geninfo:
    filename  : protos/golang.conradwood.net/apis/promconfig/promconfig.proto
    gopackage : golang.conradwood.net/apis/promconfig
    importname: ai_0
+   clientfunc: GetPromConfigService
+   serverfunc: NewPromConfigService
+   lookupfunc: PromConfigServiceLookupID
    varname   : client_PromConfigServiceClient_0
    clientname: PromConfigServiceClient
    servername: PromConfigServiceServer
@@ -33,7 +40,7 @@ func GetPromConfigClient() PromConfigServiceClient {
        return client_PromConfigServiceClient_0
     }
 
-    client_PromConfigServiceClient_0 = NewPromConfigServiceClient(client.Connect("promconfig.PromConfigService"))
+    client_PromConfigServiceClient_0 = NewPromConfigServiceClient(client.Connect(PromConfigServiceLookupID()))
     lock_PromConfigServiceClient_0.Unlock()
     return client_PromConfigServiceClient_0
 }
@@ -49,8 +56,9 @@ func GetPromConfigServiceClient() PromConfigServiceClient {
        return client_PromConfigServiceClient_0
     }
 
-    client_PromConfigServiceClient_0 = NewPromConfigServiceClient(client.Connect("promconfig.PromConfigService"))
+    client_PromConfigServiceClient_0 = NewPromConfigServiceClient(client.Connect(PromConfigServiceLookupID()))
     lock_PromConfigServiceClient_0.Unlock()
     return client_PromConfigServiceClient_0
 }
 
+func PromConfigServiceLookupID() string { return "promconfig.PromConfigService" } // returns the ID suitable for lookup in the registry. treat as opaque, subject to change.
