5185ccd83adc999489a87646f47c498081dbe5be
Conrad Wood <cnw@conradwood.net>
2024-11-12 13:02:18 +0000

    use dns names for hosts instead of ip addresses
    Change-Id: I3ed6b063744a540d373b1cdefec03c909b9fc917

---
+++ src/golang.conradwood.net/promconfig/targets/resolver.go
@@ -0,0 +1,49 @@
+package targets
+
+import (
+	"fmt"
+	"net"
+	"sort"
+	"time"
+
+	"golang.conradwood.net/go-easyops/cache"
+	"golang.conradwood.net/go-easyops/utils"
+)
+
+var (
+	hostcache = cache.New("hostcache", time.Duration(5)*time.Minute, 1000)
+)
+
+type hostcache_entry struct {
+	hostname string
+	port     uint32
+}
+
+func HostName(ip string) (string, uint32) {
+	nip, port, _, err := utils.ParseIP(ip)
+	if err != nil {
+		fmt.Printf("inv ip (%s) :%s\n", ip, err)
+		return "", 0
+	}
+	o := hostcache.Get(nip)
+	if o != nil {
+		hce := o.(*hostcache_entry)
+		return hce.hostname, hce.port
+	}
+	addr, err := net.LookupAddr(nip)
+	if err != nil {
+		fmt.Printf("Lookup failed: %s\n", err)
+		return "", 0
+	}
+	if len(addr) == 0 {
+		return "", 0
+	}
+	sort.Slice(addr, func(i, j int) bool {
+		return addr[i] < addr[j]
+	})
+	res := addr[0]
+	hce := &hostcache_entry{hostname: res, port: port}
+	hostcache.Put(nip, hce)
+	return res, port
+
+}
